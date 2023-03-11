4775e7a94e65bb4026356949c93a8f95b585830c
vanessa garcia <venessag0908@gmail.com>
2023-03-10 23:08:11 -0700

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(names, event){
+    let thanks = []
+    for(let n = 0; n < names.length; n++) {
+        thanks.push("Thank you, " + names[n] + ", for the wonderful " + event+ " gift!");
+    }
+    return thanks;
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+}
